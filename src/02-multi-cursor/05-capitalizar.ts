8bf41d6d73d59cf8213d424cac45ceaea36a2cc4
LG-Cabarg25 <lgcabarg25@gmail.com>
2024-07-07 22:53:38 -0600

    updaste repo

--- src/02-multi-cursor/05-capitalizar.ts
+++ src/02-multi-cursor/05-capitalizar.ts
@@ -19,22 +19,22 @@
 //*
 
 function capitalizados() {
-    //! Sólo la primera letra
-    const hulk       = 'brouce banner';
-    const Hawkeye    = 'cinton francis';
-    const ironman    = 'tony stark';
-    const spiderman  = 'peter parker';
-    const viudaNegra = 'natalia romanova';
-
-}
-
-function minusculas() {
-
+    // Sólo la primera letra!///
     const HULK       = 'brouce banner';
     const HAWKEYE    = 'cinton francis';
     const IRONMAN    = 'tony stark';
     const SPIDERMAN  = 'peter parker';
     const VIUDANEGRA = 'natalia romanova';
+
+}
+
+function minusculas() {
+
+    const hulk       = 'brouce banner';
+    const hawkeye    = 'cinton francis';
+    const ironman    = 'tony stark';
+    const spiderman  = 'peter parker';
+    const viudanegra = 'natalia romanova';
 
 }
 
--- src/02-multi-cursor/08-arreglos.ts
+++ src/02-multi-cursor/08-arreglos.ts
@@ -1,4 +1,4 @@
-//*
+//
 //*    Objetivo:
 //*        Crear múltiples cursores
 //*        Y usarlo para editar un arreglo de días de la semana
@@ -10,18 +10,10 @@
 //*
 
 // Crear un arreglo con los días de la semana
-lunes
-martes
-miércoles
-jueves
-viernes
-sábado
-domingo
+const diasDemmo =['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'] 
 
 
-
-
-
+const dias = ['lunes','martes ','miercoles','jueves','viernes','sabado','domingo',]
 // Objetivo final
 
 const diasDemo = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
